76d1b9eef339c18d074fd48e0363767e680e3b2c
tadangthanh81 <tadangthanh81@gmail.com>
2019-02-24 00:43:54 +0700

    branch merge team3 (entity cu)

---
+++ src/main/java/com/cmcglobal/utils/Snippet.java
@@ -0,0 +1,27 @@
+/**
+ * Create by: User - CMC
+ * Create date: Feb 24, 2019
+ * Modifier: User
+ * Modified date: Feb 24, 2019
+ * Description: ....
+ * Version 1.0
+ */
+package com.cmcglobal.utils;
+
+/**
+ * Create by: thanhtd - CMC
+ * Create date: Feb 24, 2019
+ * Modifier: thanhtd
+ * Modified date: Feb 24, 2019
+ * Description: ....
+ * Version 1.0
+ */
+public class Snippet {
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
+
